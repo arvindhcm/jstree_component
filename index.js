1aa2e013600c45ea5e5b2f79251a308bec31f33e
arvindh.cm <arvindh.cm@zohocorp.com>
2023-01-24 09:30:22 +0530

    [ENAHANCEMENT]: data updated.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let data = { name: "Root", children: [{ name: "Cars", children: [{ name: "Ferrari", children: [{ name: "red", children: [{ name: "Roma", }, { name: "Italia", }] }, { name: "yellow", }] }, { name: "Mclaren", children: [{ name: "mL1", }, { name: "mL2", }] }, { name: "Mazda", children: [{ name: "mx1", }, { name: "mx5", }] },] }, { name: "Bikes", children: [{ name: "Kawasaki", children: [{ name: "ninja600", }, { name: "ninja1000", children: [{ name: 'type_first' }, { name: 'type_second' }] }] }, { name: "Ducati", children: [{ name: "D1", }, { name: "D2", }] }] }] };
+let data = { name: "Root", children: [{ name: "Cars", children: [{ name: "Ferrari", children: [{ name: "red", children: [{ name: "Roma", }, { name: "Italia", }] }, { name: "yellow", }] }, { name: "Mclaren", children: [{ name: "spider 570s", }, { name: "spider 600", }] }, { name: "Mazda", children: [{ name: "cx1", }, { name: "mx5", }] },] }, { name: "Bikes", children: [{ name: "Kawasaki", children: [{ name: "ninja600", }, { name: "ninja1000", children: [{ name: 'type_first' }, { name: 'type_second' }] }] }, { name: "Ducati", children: [{ name: "Panigale", }, { name: "Scrambler", }] }] }] };
 let data2 = { name: "Root", children: [ { name: "apple", children: [ { name: "macbook", children: [ { name: "red", children: [ { name: "m1", }, { name: "m2", } ] }, { name: "mac mini", } ] }, ] }] };
 
 
